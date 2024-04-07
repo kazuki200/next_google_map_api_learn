d4bc759eec3c9a1a05cb30e0588de50218cfebc5
kazuki2000 <pkazuki2000@icloud.com>
2024-04-08 00:37:42 +0900

    コミット

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,8 @@ const nextConfig = {
     unoptimized: true,
     domains: ["lh3.googleusercontent.com", "maps.googleapis.com"],
   },
+  env: {
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
 };
 export default nextConfig;
